0e899a7e99baaf9dd4208fee1d460737bf190718
Isabelle <isabellebd.bandeira@gmail.com>
2024-08-29 20:12:22 -0300

    grafico consultores disponiveis

--- Front-End/js/dashboards-chart.js
+++ Front-End/js/dashboards-chart.js
@@ -1,161 +1,162 @@
 var chrt = document.getElementById("graph");
-      var graph = new Chart(chrt, {
-         type: 'pie',
-         data: {
-            labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
-            datasets: [{
-               label: "online tutorial subjects",
-               data: [9, 8, 10, 7, 6, 12],
-               backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
-               borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
-               borderWidth: 2,
-            }],
-         },
-         options: {
-            responsive: true,
-            plugins: {
-               legend: {
-                 display: false
-               },
-               title: {
-                  display: true,
-                  text: 'Custom Chart Title'
-              }
-             }
-         },
-      });
-
-      var chrt = document.getElementById("graph2");
-      var graph = new Chart(chrt, {
-         type: 'line',
-         data: {
-            labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
-            datasets: [{
-               label: "online tutorial subjects",
-               data: [9, 8, 10, 7, 6, 12],
-               backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
-               borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
-               borderWidth: 2,
-            }],
-         },
-         options: {
-            responsive: true,
-            plugins: {
-               legend: {
-                 display: false
-               },
-               title: {
-                  display: true,
-                  text: 'Custom Chart Title'
-              }
-             }
-         },
-      });
+   var graph = new Chart(chrt, {
+      type: 'bar',
+      data: {
+         labels: ["Ex", "Sr", "Pr", "Jr", "Bg"],
+         datasets: [{
+            label: "online tutorial subjects",
+            data: [9, 8, 10, 7, 6, 12],
+            backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue'],
+            borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy'],
+            borderWidth: 2,
+         }],
+      },
+      options: {
+         responsive: true,
+         plugins: {
+            legend: {
+               display: false
+            },
+            title: {
+               display: true,
+               text: 'Consultores disponíveis (por Senioridade)'
+            }
+         }
+      },
+});
       
-      var chrt = document.getElementById("graph3");
-      var graph = new Chart(chrt, {
-         type: 'bar',
-         data: {
-            labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
-            datasets: [{
-               label: "online tutorial subjects",
-               data: [9, 8, 10, 7, 6, 12],
-               backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
-               borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
-               borderWidth: 0,
-            }],
+      
+var chrt = document.getElementById("graph2");
+var graph = new Chart(chrt, {
+   type: 'bar',
+   data: {
+      labels: ["N1", "N2", "N3"],
+      datasets: [{
+         label: "online tutorial subjects",
+         data: [9, 8, 10, 7, 6, 12],
+         backgroundColor: ['yellow', 'aqua', 'pink'],
+         borderColor: ['red', 'blue', 'fuchsia'],
+         borderWidth: 2,
+      }],
+   },
+   options: {
+      responsive: true,
+      plugins: {
+         legend: {
+            display: false
          },
-         options: {
-            responsive: true,
-            plugins: {
-               legend: {
-                 display: false
-               },
-               title: {
-                  display: true,
-                  text: 'Custom Chart Title'
-              }
-             }
-         },
-      });
+         title: {
+            display: true,
+            text: 'Consultores disponíveis (por Complexidade)'
+         }
+         }
+   },
+});
 
-      var chrt = document.getElementById("graph4");
-      var graph = new Chart(chrt, {
-         type: 'bar',
-         data: {
-            labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
-            datasets: [{
-               label: "online tutorial subjects",
-               data: [9, 8, 10, 7, 6, 12],
-               backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
-               borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
-               borderWidth: 0,
-            }],
+var chrt = document.getElementById("graph3");
+var graph = new Chart(chrt, {
+   type: 'bar',
+   data: {
+      labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
+      datasets: [{
+         label: "online tutorial subjects",
+         data: [9, 8, 10, 7, 6, 12],
+         backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
+         borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
+         borderWidth: 0,
+      }],
+   },
+   options: {
+      responsive: true,
+      plugins: {
+         legend: {
+            display: false
          },
-         options: {
-            responsive: true,
-            plugins: {
-               legend: {
-                 display: false
-               },
-               title: {
-                  display: true,
-                  text: 'Custom Chart Title'
-              }
-             }
-         },
-      });
+         title: {
+            display: true,
+            text: 'Custom Chart Title'
+         }
+         }
+   },
+});
 
-      var chrt = document.getElementById("graph5");
-      var graph = new Chart(chrt, {
-         type: 'bar',
-         data: {
-            labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
-            datasets: [{
-               label: "online tutorial subjects",
-               data: [9, 8, 10, 7, 6, 12],
-               backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
-               borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
-               borderWidth: 0,
-            }],
+var chrt = document.getElementById("graph4");
+var graph = new Chart(chrt, {
+   type: 'bar',
+   data: {
+      labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
+      datasets: [{
+         label: "online tutorial subjects",
+         data: [9, 8, 10, 7, 6, 12],
+         backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
+         borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
+         borderWidth: 0,
+      }],
+   },
+   options: {
+      responsive: true,
+      plugins: {
+         legend: {
+            display: false
          },
-         options: {
-            responsive: true,
-            plugins: {
-               legend: {
-                 display: false
-               },
-               title: {
-                  display: true,
-                  text: 'Custom Chart Title'
-              }
-             }
-         },
-      });
+         title: {
+            display: true,
+            text: 'Custom Chart Title'
+         }
+         }
+   },
+});
 
-      var chrt = document.getElementById("graph6");
-      var graph = new Chart(chrt, {
-         type: 'bar',
-         data: {
-            labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
-            datasets: [{
-               label: "online tutorial subjects",
-               data: [9, 8, 10, 7, 6, 12],
-               backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
-               borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
-               borderWidth: 0,
-            }],
+var chrt = document.getElementById("graph5");
+var graph = new Chart(chrt, {
+   type: 'bar',
+   data: {
+      labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
+      datasets: [{
+         label: "online tutorial subjects",
+         data: [9, 8, 10, 7, 6, 12],
+         backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
+         borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
+         borderWidth: 0,
+      }],
+   },
+   options: {
+      responsive: true,
+      plugins: {
+         legend: {
+            display: false
          },
-         options: {
-            responsive: true,
-            plugins: {
-               legend: {
-                 display: false
-               },
-               title: {
-                  display: true,
-                  text: 'Custom Chart Title'
-              }
-             }
+         title: {
+            display: true,
+            text: 'Custom Chart Title'
+         }
+         }
+   },
+});
+
+var chrt = document.getElementById("graph6");
+var graph = new Chart(chrt, {
+   type: 'bar',
+   data: {
+      labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
+      datasets: [{
+         label: "online tutorial subjects",
+         data: [9, 8, 10, 7, 6, 12],
+         backgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
+         borderColor: ['red', 'blue', 'fuchsia', 'green', 'navy', 'black'],
+         borderWidth: 0,
+      }],
+   },
+   options: {
+      responsive: true,
+      plugins: {
+         legend: {
+            display: false
          },
-      });
+         title: {
+            display: true,
+            text: 'Custom Chart Title'
+         }
+         }
+   },
+});

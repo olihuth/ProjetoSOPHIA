d7c809a9c416713a273f9f81496ea000ebc6990e
Isabelle <isabellebd.bandeira@gmail.com>
2024-08-29 22:59:11 -0300

    filter btn na listaProjetos

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -15,3 +15,21 @@ function resetEditPerfilForm(){
     document.getElementById("editPerfilForm").reset();
 }
 
+function toggleFilterDropdown(){
+    // document.getElementById("dropdownFilter");
+    var element = document.getElementById("dropdownFilter");
+    var hidden = element.getAttribute("hidden");
+
+    console.log("whats wrong?");
+
+    if (hidden) {
+       element.removeAttribute("hidden");
+       console.log("hidden = " + hidden);
+    //    button.innerText = "Hide div";
+    } else {
+       element.setAttribute("hidden", "hidden");
+       console.log("hidden = " + hidden);
+    //    button.innerText = "Show div";
+    }
+}
+
